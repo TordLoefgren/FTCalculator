91d20e965cae235d4cf8b544617c4851e861a007
Tord L <tord0505@hotmail.com>
2023-06-21 13:53:58 +0200

    Added simple WPF UI layout and created first service interface.

---
+++ FTCalculator/Models/OperationService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class OperationService : IOperationService
+{
+    public float Add(float valueOne, float valueTwo)
+    {
+        return valueOne + valueTwo;
+    }
+
+    public float Divide(float valueOne, float valueTwo)
+    {
+        if (valueTwo == 0)
+        {
+            // Do nothing in UI?
+            throw new DivideByZeroException();
+        }
+        return valueOne / valueTwo;
+    }
+
+    public float Mutiply(float valueOne, float valueTwo)
+    {
+        return valueOne * valueTwo;
+    }
+
+    public float Subtract(float valueOne, float valueTwo)
+    {
+        return valueOne - valueTwo;
+    }
+}
---
+++ FTCalculator/Services/IOperationService.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public interface IOperationService
+{
+    float Add(float valueOne, float valueTwo);
+    float Subtract(float valueOne, float valueTwo);
+    float Divide(float valueOne, float valueTwo);
+    float Mutiply(float valueOne, float valueTwo);
+}
